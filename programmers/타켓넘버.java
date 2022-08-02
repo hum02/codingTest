ea9bfc6a22d1289d4079429628b81a1747def19b
hum02 <ehdgma02@gmail.com>
2022-08-02 20:39:07 +0900

    타겟넘버

---
+++ programmers/타켓넘버.java
@@ -0,0 +1,26 @@
+class Solution {
+    int answer=0;
+    int[] nums;
+    int target;
+
+    public void dfs(int idx,int sum){
+        if(idx==nums.length){
+            if(sum==target){
+                ++answer;
+            }
+            return;
+        }
+
+        dfs(idx+1,sum-nums[idx]);
+        dfs(idx+1,sum+nums[idx]);
+    }
+
+    public int solution(int[] numbers, int target) {
+        nums=numbers;
+        this.target=target;
+
+        dfs(0,0);
+
+        return answer;
+    }
+}
