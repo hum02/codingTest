eced53f44430562e9de14d6c5714d8ff4ff48c49
hum02 <ehdgma02@gmail.com>
2022-08-02 20:48:46 +0900

    폰켓몬

---
+++ programmers/폰켓몬.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> set= new HashSet<>();
+
+
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+
+        answer=set.size();
+        if(answer>nums.length/2){
+            answer=nums.length/2;
+        }
+
+        return answer;
+    }
+}
