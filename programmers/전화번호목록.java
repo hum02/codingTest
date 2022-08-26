d17cad0a4213e5670eb3ed44335ad09387ae0836
hum02 <ehdgma02@gmail.com>
2022-08-26 09:24:55 +0900

    전화번호목록

---
+++ programmers/전화번호목록.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashSet<String> set= new HashSet<>();
+
+        for(int i=0;i<phone_book.length;i++){
+            set.add(phone_book[i]);
+        }
+
+        for(int j=0;j<phone_book.length;j++){
+            for(int k=0;k<phone_book[j].length();k++){
+                if(set.contains(phone_book[j].substring(0,k))){
+                    answer=false;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
